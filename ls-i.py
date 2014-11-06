37d87bf71ddef3bd9316f1af4859bc56ec94a363
yiranli <yiranli@hustunique.com>
2014-11-06 22:43:19 +0800

    替代ls -i的程序

---
+++ ls-i.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# coding = utf-8
+# Filename:ls-i.py
+import os
+l = raw_input('list:')
+list = os.listdir(l)
+list.sort()
+for i in list: 
+ if(i[0] == '.'):
+  pass
+ else:
+  print os.stat(i).st_ino,i
