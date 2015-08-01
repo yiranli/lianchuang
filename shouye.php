5eab8fe0084886ba2c934fb53800e92ca36afb3d
root <root@localhost.localdomain>
2015-08-01 09:58:33 +0800

    首页

---
+++ shouye.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+    <form action="report.html" method="post">
+    <input type="submit" value="招新报名">
+    </form>
+    <form action="deng.php" method="post">
+    <input type="submit" value="查询" />
+    </form>
+</body>
+</html>
